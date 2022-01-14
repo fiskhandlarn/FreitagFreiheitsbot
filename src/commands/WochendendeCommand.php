73e354241e9d6df3448665f5a9c792aa5f76d094
fiskhandlarn <oskar@joelson.org>
2022-01-14 07:51:39 +0100

    Try again without URL

--- src/commands/WochendendeCommand.php
+++ src/commands/WochendendeCommand.php
@@ -46,8 +46,9 @@ class WochenendeCommand extends SystemCommand
     public function execute(): ServerResponse
     {
         return $this->replyToChat(
-            'Ja, saufen!' . PHP_EOL . PHP_EOL .
-            'https://www.youtube.com/watch?v=3aGf0t69_xk'
+            'Ja, saufen!'
+            //. PHP_EOL . PHP_EOL .
+            //'https://www.youtube.com/watch?v=3aGf0t69_xk'
         );
     }
 }
