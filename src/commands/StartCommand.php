ecb1aa85d643820637aa238af01bf31523b223ad
fiskhandlarn <oskar@joelson.org>
2022-01-14 07:42:54 +0100

    Add more info

--- src/commands/StartCommand.php
+++ src/commands/StartCommand.php
@@ -55,8 +55,9 @@ class StartCommand extends SystemCommand
         // $deep_linking_parameter = $this->getMessage()->getText(true);
 
         return $this->replyToChat(
-            'Ist heute Freitag?' . PHP_EOL .
-            '🤷'
+            'Ist heute Freitag? 🤷' . PHP_EOL . PHP_EOL .
+            '/start Welcher Tag ist es?' . PHP_EOL .
+            '/wochendene' .
         );
     }
 }
