ebbd05882afd0a585797c6e99978a34a772a1ebf
fiskhandlarn <oskar@joelson.org>
2022-01-14 07:57:31 +0100

    Fix PHP error

--- src/commands/WochendendeCommand.php
+++ src/commands/WochendendeCommand.php
@@ -50,7 +50,7 @@ class WochenendeCommand extends SystemCommand
 
         if ($weekday === 5 || $weekday === 6 || $weekday === 7) {
             return $this->replyToChat(
-                'Ja, saufen!' . PHP_EOL
+                'Ja, saufen!' . PHP_EOL .
                 'https://www.youtube.com/watch?v=3aGf0t69_xk'
             );
         } else {
